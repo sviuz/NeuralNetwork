f4ceae93e7767af9a83e1e9551003cdc7458df60
sviuz <kuibidanill@gmail.com>
2020-11-12 22:58:58 +0200

    Add project files.

---
+++ NeuralNetwork/Neuron.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        public List<double> Weights{ get; }
+
+        public NeuronType NeuronType { get; }
+
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount,NeuronType type = NeuronType.Normal)
+        {
+            NeuronType = type;
+            Weights = new List<double>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                Weights.Add(1);
+            }
+        }
+
+
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            Output = Sigm(sum);
+
+            return Output;
+        }
+
+        private double Sigm(double x)
+        {
+            return 1.0 / (1.0 + Math.Pow(Math.E, -x));
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+
+
+    }
+}
---
+++ NeuralNetwork/NeuronType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    public enum NeuronType
+    {
+        Input = 0,
+        Normal = 1,
+        Output = 2
+    }
+}
